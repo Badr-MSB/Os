7e11e6bbf4859c73f35fc0b3c72fdfe95d9db5c2
Badr-MSB <badrmesbahi29@gmail.com>
2024-06-30 04:39:56 +0200

    list all pci devices

--- pcie/pci.c
+++ pcie/pci.c
@@ -3,8 +3,10 @@
 #include "../lib/stdio.h"
 
 void list_pci_devices(void){
-    uint16_t* vendor_id = (uint16_t*)VIRT_PCIE_ECAM;
-    printf("Display PCI Devices");
-    printf("Vendor id : 0x%x\n", vendor_id[0]);
-    printf("Device id : 0x%x\n", vendor_id[1]);
+    int block = 0;
+    uint16_t* base_add = (uint16_t*)VIRT_PCIE_ECAM;
+    while( *base_add != 0xffff  ){
+        printf("vandor : 0x%x  &  Device : 0x%x\n",base_add[0] ,base_add[1] );
+        base_add += 4*4096;
+    }
 }
